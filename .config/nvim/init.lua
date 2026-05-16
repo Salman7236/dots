7a16cd8cc4f84f911206a28acae1c73ff09a3433
Salman <muhammadsalman7236@gmail.com>
2026-05-16 14:05:42 +0500

    Add Neovim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,18 @@
+vim.loader.enable()
+
+require('config.options')
+require('config.keybinds')
+require('config.diagnostics')
+require('plugins.transparent')
+
+  -- [[ Basic Autocommands ]]
+  --  See `:help lua-guide-autocommands`
+
+  -- Highlight when yanking (copying) text
+  --  Try it with `yap` in normal mode
+  --  See `:help vim.hl.on_yank()`
+  vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function() vim.hl.on_yank() end,
+  })
---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,57 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.g.have_nerd_font = true
+vim.o.mouse = 'a'
+vim.opt.cursorline = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.termguicolors = true
+vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+vim.opt.scrolloff = 10
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = false
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.o.breakindent = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.signcolumn = "yes"
+-- vim.opt.colorcolumn = "100"
+vim.opt.showmatch = true
+vim.opt.cmdheight = 1
+-- vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
+vim.opt.showmode = false
+vim.o.undofile = true
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.list = true
+vim.opt.listchars = {
+  tab = "» ",
+  trail = "·",
+  nbsp = "␣",
+}
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.confirm = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+-- vim.opt.pumheight = 10
+-- vim.opt.pumblend = 10
+-- vim.opt.winblend = 0
+-- vim.opt.conceallevel = 0
+-- vim.opt.concealcursor = ""
+-- vim.opt.synmaxcol = 300
+-- vim.opt.fillchars = { eob = " " }
+vim.opt.autoread = true
+vim.opt.autowrite = false
+vim.opt.hidden = true
+vim.opt.backspace = { "indent", "eol", "start" }
+-- vim.opt.autochdir = false
+-- vim.opt.iskeyword:append("-")
+-- vim.opt.path:append("**")
+vim.opt.wildmenu = true
+vim.opt.wildmode = "longest:full,full"
+vim.opt.diffopt:append("linematch:60")
+-- vim.opt.redrawtime = 10000
+-- vim.opt.maxmempattern = 20000
