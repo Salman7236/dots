cc7631954eb080d150ac457ccbc427791773ef41
Salman <muhammadsalman7236@gmail.com>
2026-05-19 03:22:43 +0500

    Disable transparent.nvim due to conflicts with mini.statusline

--- .config/nvim/lua/plugins/transparent.lua
+++ .config/nvim/lua/plugins/transparent.lua
@@ -1,20 +1,20 @@
-vim.pack.add({
-  "https://github.com/xiyaowong/transparent.nvim",
-})
-
-require("transparent").setup({
-  exclude_groups = {
-    "CursorLine",
-    "CursorLineNr",
-    "SignColumn",
-  },
-})
-
-vim.api.nvim_set_hl(0, "CursorLine", {
-  bg = "#24283b",
-})
-
-vim.api.nvim_set_hl(0, "CursorLineNr", {
-  fg = "#ff9e64",
-  bold = true,
-})
+-- vim.pack.add({
+--   "https://github.com/xiyaowong/transparent.nvim",
+-- })
+--
+-- require("transparent").setup({
+--   exclude_groups = {
+--     "CursorLine",
+--     "CursorLineNr",
+--     "SignColumn",
+--   },
+-- })
+--
+-- vim.api.nvim_set_hl(0, "CursorLine", {
+--   bg = "#24283b",
+-- })
+--
+-- vim.api.nvim_set_hl(0, "CursorLineNr", {
+--   fg = "#ff9e64",
+--   bold = true,
+-- })
