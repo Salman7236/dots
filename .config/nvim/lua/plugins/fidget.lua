e7028642861293c4a31d196db1a133c9d71c33e8
Salman <muhammadsalman7236@gmail.com>
2026-05-29 17:12:39 +0500

    nvim(fidget.nvim): enable vim.notify override for LSP/system alerts

--- .config/nvim/lua/plugins/fidget.lua
+++ .config/nvim/lua/plugins/fidget.lua
@@ -1,3 +1,10 @@
-  -- Useful status updates for LSP.
-  vim.pack.add { 'https://github.com/j-hui/fidget.nvim' }
-  require('fidget').setup {}
+-- Useful status updates for LSP.
+vim.pack.add({ "https://github.com/j-hui/fidget.nvim" })
+
+require("fidget").setup({
+	notification = {
+		override_vim_notify = true,
+	},
+})
+
+vim.notify = require("fidget.notification").notify
